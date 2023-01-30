3537eb52bd3ca8ba9dd1a20e3a12c6e896a9f412
Andy LaBorde <32881770+AndyLaBorde@users.noreply.github.com>
2023-01-30 12:52:24 -0700

    fixed card image bugs, arranged projects newest first

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -17,9 +17,39 @@ function Projects() {
             <div id="full-stack-apps" className='col-4'>
                 <h5 className='text-info'>Full-Stack</h5>
                 <div id="projectScroll" className='row customScroll'S>
+                    <Card bg="secondary" text="light" style={{ width: "18rem" }} className="m-1 align-items-center justify-content-center">
+                        <Card.Body>
+                            <Card.Img src={Frogr}></Card.Img>
+                            <Card.Title className="p-2"> Frogr </Card.Title>
+                            <Card.Text className="p-2"> A Microblog where you can upload images, videos, texts or links to share with the frogr community! Frogr was built using React, Express, GraphQL(Apollo), Node.js, and MongoDB in a 5 day sprint. Agile methodologies, Kanban, and daily Scrum meetings kept the team on task individually and as a group. 
+                            </Card.Text>
+                            <Card.Link href="https://frogr-app.herokuapp.com/" className="p-2 text-decoration-none text-info"> 
+                                <img src={chromeIcon} alt="chrome icon"></img>
+                            </Card.Link>
+                            <Card.Link href="https://github.com/cbaird21/frogr" className="ml-2" target="no_blank">
+                                <img src={gitHubIcon} alt="repo link"></img>
+                            </Card.Link>
+                        </Card.Body>
+                    </Card>
+                    
+
+                    <Card bg="secondary" text="light" style={{ width: "18rem" }} className="m-1 align-items-center justify-content-center">
+                        <Card.Body>
+                            <Card.Img src={Pawsome} alt="pawsome image"></Card.Img>
+                            <Card.Title className="p-2"> Pawsome </Card.Title>
+                            <Card.Text className="p-2"> The social media for pets!  This application was created using Javascript, SQL, Node.js and Handlebars in a 7 day sprint. Agile methodologies, Kanban, and daily Scrum meetings led to a succesfull production environment. </Card.Text>
+                            <Card.Link href="https://pawsome.herokuapp.com/" className="p-2 text-decoration-none text-info"> 
+                                <img src={chromeIcon} alt="chrome icon"></img>
+                            </Card.Link>
+                            <Card.Link href="https://github.com/AndyLaBorde/Pawsome" className="ml-2" target="no_blank">
+                                <img src={gitHubIcon} alt="repo link"></img>
+                            </Card.Link>
+                        </Card.Body>
+                    </Card>
+
                     <Card bg="secondary" text="light" style={{ width: "18rem" }} className="m-1 align-items-center ">
                         <Card.Body>
-                            <Card.Img src={gQuery}></Card.Img>
+                            <Card.Img src={gQuery} alt="gQuery gif"></Card.Img>
                             <Card.Title className="p-2"> G-Query </Card.Title>
                             <Card.Text className="p-2">
                                 With G-Query a user can search by genre, platform, and the game name itself. Using multiple custom built API's from rawg.io and youtube.com to display searched game information, and video trailers to their respective games.
@@ -27,36 +57,7 @@ function Projects() {
                             <Card.Link href="https://andylaborde.github.io/G-Query/" className="p-2 text-decoration-none text-info" target="no_blank"> 
                                 <img src={chromeIcon} alt="chrome icon"></img>
                             </Card.Link>
-                            <Card.Link href="https://github.com/AndyLaBorde/G-Query" classname="ml-2" target="no_blank">
-                                <img src={gitHubIcon} alt="repo link"></img>
-                            </Card.Link>
-                        </Card.Body>
-                    </Card>
-
-                    <Card bg="secondary" text="light" style={{ width: "18rem" }} className="m-1 align-items-center justify-content-center">
-                        <Card.Body>
-                            <Card.Img src="">{Pawsome}</Card.Img>
-                            <Card.Title className="p-2"> Pawsome </Card.Title>
-                            <Card.Text className="p-2"> The social media for pets!  This application was created using Javascript, SQL, Node.js and Handlebars in a 7 day sprint. Agile methodologies, Kanban, and daily Scrum meetings led to a succesfull production environment. </Card.Text>
-                            <Card.Link href="https://pawsome.herokuapp.com/" className="p-2 text-decoration-none text-info"> 
-                                <img src={chromeIcon} alt="chrome icon"></img>
-                            </Card.Link>
-                            <Card.Link href="https://github.com/AndyLaBorde/Pawsome" classname="ml-2" target="no_blank">
-                                <img src={gitHubIcon} alt="repo link"></img>
-                            </Card.Link>
-                        </Card.Body>
-                    </Card>
-
-                    <Card bg="secondary" text="light" style={{ width: "18rem" }} className="m-1 align-items-center justify-content-center">
-                        <Card.Body>
-                            <Card.Img src="">{Frogr}</Card.Img>
-                            <Card.Title className="p-2"> Frogr </Card.Title>
-                            <Card.Text className="p-2"> A Microblog where you can upload images, videos, texts or links to share with the frogr community! Frogr was build using React, Express, GraphQL(Apollo), Mongoose, Node.js, and MongoDB in a 5 day sprint. Agile methodologies, Kanban, and daily Scrum meetings kept the team on task individually and as a group. 
-                            </Card.Text>
-                            <Card.Link href="https://frogr-app.herokuapp.com/" className="p-2 text-decoration-none text-info"> 
-                                <img src={chromeIcon} alt="chrome icon"></img>
-                            </Card.Link>
-                            <Card.Link href="https://github.com/cbaird21/frogr" classname="ml-2" target="no_blank">
+                            <Card.Link href="https://github.com/AndyLaBorde/G-Query" className="ml-2" target="no_blank">
                                 <img src={gitHubIcon} alt="repo link"></img>
                             </Card.Link>
                         </Card.Body>
